a0f9a9df900e5dc71ee0af69b711347ac95d264b
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-02-15 16:07:42 +0000

    PLATOPS-1850: Library security upgrades

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -7,12 +7,22 @@ private object LibDependencies {
   val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
     shared = Seq(
       "ch.qos.logback" % "logback-classic" % "1.2.3",
-      "org.pegdown" % "pegdown" % "1.6.0"
+      "org.pegdown" % "pegdown" % "1.6.0",
+      // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
+      "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7",
+      "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7",
+      "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7",
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7",
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7"
     ),
     play25 = Seq(
       "com.typesafe.play" %% "play-ws" % play25Version % "provided",
       "com.typesafe.play" %% "play-test" % play25Version,
-      "org.scalatest" %% "scalatest" % "3.0.5"
+      "org.scalatest" %% "scalatest" % "3.0.5",
+      // force dependencies due to security flaws found in xercesImpl 2.11.0
+      // only applies to play 2.5 since it was removed from play 2.6
+      // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
+      "xerces" % "xercesImpl" % "2.12.0"
     ),
     play26 = Seq(
       "com.typesafe.play" %% "play-ahc-ws" % play26Version % "provided",
